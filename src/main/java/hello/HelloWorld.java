0bdfa1c2efe04b3b7bd02131839f2f46b5b3b5af
warrenellerbeck <warren.ellerbeck@gmail.com>
2021-06-01 12:01:08 -0700

    whateva

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime + " if you can be arsed!!!");
+		  System.out.println("The current local time is: " + currentTime + " ......whateva");
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
